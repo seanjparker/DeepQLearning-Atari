cf00b611aa589c44f98ee6818804822ca704fecd
Sean Parker <sean.parker067@gmail.com>
2020-03-08 19:48:04 +0000

    Add double dqn support

--- agents/dueling_dqn/train.py
+++ agents/dueling_dqn/train.py
@@ -2,7 +2,7 @@ from dueling_dqn.ddqn_train import train_model
 from utils.atari import construct_env
 
 
-def train():
+def train(use_double_dqn=False):
     env = construct_env('BreakoutNoFrameskip-v4', frame_stack=True,
                         record_video=True, record_video_steps=500, frame_skip=4)
 
@@ -20,7 +20,8 @@ def train():
         learning_starts=10000,
         target_network_update_freq=1000,
         gamma=0.99,
-        checkpoint_path='./checkpoints/'
+        checkpoint_path='./checkpoints/',
+        double_dqn=use_double_dqn
     )
 
     model.save('./saved_model/ddqn_breakout/')
